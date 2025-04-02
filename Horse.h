c63b04714992222fb53f7fe7e6cde37f035763ad
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-04-02 22:28:21 +0000

    commit

---
+++ Horse.h
@@ -0,0 +1,16 @@
+#ifndef HORSE_H_EXISTS
+#define HORSE_H_EXISTS
+
+#include <string>
+
+class Horse{
+	protected:
+		int position;
+	public:
+		int getPosition;
+		int setPosition;
+		int forward;
+		bool isWinner;
+};
+
+#endif
