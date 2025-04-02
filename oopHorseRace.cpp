595250aa1089e303e1dee0d7105aeccf4a7a709a
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-04-02 21:55:10 +0000

    commit

--- oopHorseRace.cpp
+++ oopHorseRace.cpp
@@ -13,43 +13,43 @@ int keepGoing;
 int name[] = {1,2,3,4,5};
 
 int main(){
-	stand(time(NULL));
+	srand(time(NULL));
 	int position = 0;
 	int length = 0;
 
 	Race race;
-	for (int i = 0; I < 5; ++i)
-{
+	
+	for (int i = 0; I < 5; ++i){
 
 	race.length = length;
 	race.printLane(position, name[I]);
 
-}
-
-keepGoing = TRUE;
-while (keepGoing == TRUE){
-
-	for (int i = 0; I < 1; ++i){
-	race.length = race.length + forward(position);
-	cout << "Race. length: " << race.length << endl;
-
-	race.printLane(race.length,name[0]);
-	length = 0;
-
-	cout << name[0] << " Position: " << forward(position) << endl;
-
 	}
 
-	for (int i = 0; I < 1; ++i){
-		race.length = race.length + advance(position);
-		cout << "Race.length: " << race.length << endl;
-		race.printLane(race.length,name[1]);
-		length = 0;
-		cout << name[1] << " Position: " << advance(position) << endl;
+		keepGoing = TRUE;
+		while (keepGoing == TRUE){
+
+		for (int i = 0; I < 1; ++i){
+			race.length = race.length + forward(position);
+			cout << "Race. length: " << race.length << endl;
+
+			race.printLane(race.length,name[0]);
+			length = 0;
+
+			cout << name[0] << " Position: " << forward(position) << endl;
+
+		}
+
+		for (int i = 0; I < 1; ++i){
+			race.length = race.length + advance(position);
+			cout << "Race.length: " << race.length << endl;
+			race.printLane(race.length,name[1]);
+			length = 0;
+			cout << name[1] << " Position: " << advance(position) << endl;
+		}
+
+		cin.ignore();
 	}
 
-	cin.ignore();
-}
-
-return 0;
+	return 0;
 }
