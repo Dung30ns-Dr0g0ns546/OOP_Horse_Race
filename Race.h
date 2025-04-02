a1cd260887db714d669e4be7fdd32aaafb931b7b
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-04-02 22:38:50 +0000

    commit

---
+++ Race.h
@@ -0,0 +1,14 @@
+#ifndef RACE_H_EXISTS
+#define RACE_H_EXISTS
+
+#include <string>
+
+class Race{
+	protected:
+		Horse* race;
+	public:
+		int length;
+		void printLane(int position, int name);
+		void Race::printLane(int length, name);
+		void Race::begin();
+};
